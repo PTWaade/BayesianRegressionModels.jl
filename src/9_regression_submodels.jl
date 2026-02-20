177d2722923f51eed65af86a0915b31ba0da77fe
Penelope Yong <penelopeysm@gmail.com>
2026-02-20 14:37:38 +0000

    Reorganise into package

--- src/9_regression_submodels.jl
+++ src/9_regression_submodels.jl
@@ -1,8 +1,67 @@
+##############################
+### PREDICTOR UPDATE TYPES ###
+##############################
+## 1. Abstract type for specifying how to update the RegressionPredictors after an operation ##
+abstract type AbstractPredictorUpdate end
+
+## 2. Concrete type and dispatch for not updating the RegressionPredictors ##
+struct NoPredictorUpdate <: AbstractPredictorUpdate end
+
+function update_predictors!(
+    predictors::Tpredictors,
+    generated_values::Tgenerated_values,
+    update_info::NoPredictorUpdate
+) where {Tpredictors<:AbstractVector{<:RegressionPredictors},Tgenerated_values<:AbstractVector}
+
+    return nothing
+
+end
+
+## 3. Concrete type and dispatch for updating the RegressionPredictors ##
+struct UpdatePredictors <: AbstractPredictorUpdate
+    term_label::Symbol
+    regression_labels::Vector{Symbol}
+end
+function update_predictors!(
+    predictors::Tpredictors,
+    generated_values::Tgenerated_values,
+    update_info::UpdatePredictors
+) where {Tpredictors<:AbstractVector{<:RegressionPredictors},Tgenerated_values<:AbstractVector}
+
+    update_predictors!(predictors, update_info.term_label, generated_values, update_info.regression_labels)
+
+end
+
+## 1. Abstract types for different operations in the Turing model ###
+abstract type AbstractRegressionOperation end
+
+## 2. Concrete dispatch type for perforning a linear combination of predictors and coefficients ##
+struct LinearCombination <: AbstractRegressionOperation end
+
+## 3. Abstract type for using Turing submodels ###
+abstract type AbstractRegressionSubmodel <: AbstractRegressionOperation end
+
+## 4. Container struct for the operation and update information
+struct RegressionOperation{Toperation<:AbstractRegressionOperation,Tupdate<:AbstractPredictorUpdate}
+    operation_info::Toperation
+    predictor_update::Tupdate
+    store_outcome::Bool
+
+    function RegressionOperation(
+        operation::Toperation,
+        predictor_update::Tupdate=NoPredictorUpdate();
+        store_outcome::Bool=false
+    ) where {Toperation<:AbstractRegressionOperation,Tupdate<:AbstractPredictorUpdate}
+        return new{Toperation,Tupdate}(operation, predictor_update, store_outcome)
+    end
+end
+
+
 ######################################
 ### SIMPLE DISTRIBUTION LIKELIHOOD ###
 ######################################
 ## 1. Concrete type for carrying information about the single distribution likelihood ##
-Base.@kwdef struct DistributionLikelihood{T<:Distribution, Targs<:NamedTuple, Tkwargs<:NamedTuple, Tobservations} <: AbstractRegressionSubmodel
+Base.@kwdef struct DistributionLikelihood{T<:Distribution,Targs<:NamedTuple,Tkwargs<:NamedTuple,Tobservations} <: AbstractRegressionSubmodel
     #The distribution type to be used
     dist::Type{T}
 
@@ -16,186 +75,5 @@ Base.@kwdef struct DistributionLikelihood{T<:Distribution, Targs<:NamedTuple, Tk
     observations::Tobservations
 end
 
-## 2. Submodel for evaluating the likelihood of observations, parameterising a distribution with predictions, a prior, or pre-specified values ##
-@model function regression_submodel(
-    likelihood_info::Tlikelihood,
-    predictions::Tpredictions,
-    predictors::Tpredictors,
-) where {Tlikelihood<:DistributionLikelihood, Tpredictions<:NamedTuple, Tpredictors<:AbstractVector{<:RegressionPredictors}}
-
-    ## 1. Extract observations ##
-    #If the observations are a pre-specified array
-    if likelihood_info.observations isa AbstractArray
-
-        #Use them as they are
-        observations = likelihood_info.observations
-
-    #If they are to be extracted from the predictors
-    elseif likelihood_info.observations isa ExtractPredictors
-
-        #Extract the corresponding, transformed, predictors
-        observations = get_predictor_values(predictors, likelihood_info.observations)
-
-    #If the observations come from the linear predictions
-    elseif likelihood_info.observations isa ExtractOutcome
-
-        #Extract the corresponding, transformed, predictions
-        observations = likelihood_info.observations.inv_link.(getproperty(likelihood_info, likelihood_info.observations.regression_label))
-
-    #If the observations are to be sampled from a distribution
-    elseif likelihood_info.observations isa Distribution
-
-        #Sample the parameter using :observations as prefix
-        observations ~ to_submodel(prefix(sample_dist(likelihood_info.observations), :observations), false)
-
-    end
-
-    # 2. Find the number of observations in the observations
-    n_observations = size(observations)[end]
-
-    # 3. Go through every positional argument and corresponding key for the distribution
-    dist_args = NamedTuple(
-        begin
-
-            #If the argument values are in the linear predictions
-            if val isa ExtractOutcome
-
-                #Extract and transform them
-                arg = val.inv_link.(getproperty(predictions, val.regression_label))
-
-            #If the argument values are to be extracted from the predictors
-            elseif val isa ExtractPredictors
-
-                #Extract the corresponding, transformed, predictors
-                arg = get_predictor_values(predictors, val)
-
-            #If a prior distribution has been specified
-            elseif val isa Distribution 
-
-                #Sample the parameter using the key as a prefix
-                param ~ to_submodel(prefix(sample_dist(val), key), false)
-
-                #Use it for every observation
-                arg = fill(param..., n_observations)
-
-            #If a vector/scalar of values has been specified
-            elseif val isa AbstractArray
-                
-                #Just use it as it is
-                arg = val
-
-            #If the argument is a single scalar
-            elseif val isa Real
-
-                #Use the same value for all observations
-                arg = fill(val, n_observations)
-
-            else
-
-                @error "A distribution argument of type $(typeof(val)) was passed to the DistributionLikelihood. This is not supported."
-
-            end
-
-            #For matrices with only one dimension
-            if size(arg, 2) == 1
-                #Make them vectors
-                arg = vec(arg)
-            else
-                #Make other matrices into vectors of vectors
-                arg = collect(eachrow(arg))
-            end
-
-            #Store the positional argument
-            key => arg
-            
-        end
-
-        for (key, val) in pairs(likelihood_info.dist_args)
-    )
-
-    # 4. Go through every keyword argument and corresponding key for the distribution
-    dist_kwargs = NamedTuple(
-        begin
-
-            #If the argument values are in the linear predictions
-            if val isa ExtractOutcome
-
-                #Extract and transform them
-                arg = val.inv_link.(getproperty(predictions, val.regression_label))
-
-            #If the argument values are to be extracted from the predictors
-            elseif val isa ExtractPredictors
-
-                #Extract the corresponding, transformed, predictors
-                arg = get_predictor_values(predictors, val)
-
-            #If a prior distribution has been specified
-            elseif val isa Distribution 
-
-                #Sample the parameter using the key as a prefix
-                param ~ to_submodel(prefix(sample_dist(val), key), false)
-
-                #Use it for every observation
-                arg = fill(param..., n_observations)
-
-            #If a vector/scalar of values has been specified
-            elseif val isa AbstractArray
-                
-                #Just use it as it is
-                arg = val
-
-            #If the argument is a single scalar
-            elseif val isa Real
-
-                #Use the same value for all observations
-                arg = fill(val, n_observations)
-
-            else
-
-                @error "A distribution argument of type $(typeof(val)) was passed to the DistributionLikelihood. This is not supported."
-
-            end
-
-            #For matrices with only one dimension
-            if size(arg, 2) == 1
-                #Make them vectors
-                arg = vec(arg)
-            else
-                #Make other matrices into vectors of vectors
-                arg = collect(eachrow(arg))
-            end
-
-            #Store the keyword argument
-            key => arg
-            
-        end
-
-        for (key, val) in pairs(likelihood_info.dist_kwargs)
-    )
-
-    # 5. Evaluate corresponding likelihood distributions for each outcome
-    imputed_observations ~ to_submodel(evaluate_observations(
-            observations, 
-            product_distribution(likelihood_info.dist.(dist_args...; dist_kwargs...))
-        ), false)
-
-    # 6. Return (potentially imputed) observations
-    return imputed_observations
-
-end
-
-## 3. Convenience submodel to allow sampling inside loops ##
-@model function sample_dist(dist)
-
-    return val ~ dist
-
-end
-
-## 4. Convenience submodel to allow evaluating the observations (they have to be a model argument)
-@model function evaluate_observations(observations::Tobservations, dist::Tdist) where {Tobservations<:AbstractArray, Tdist <: Distribution}
-
-    observations ~ dist
-
-    return observations
-
-end
+# Extended in TuringExt
+function regression_model end
--- src/BayesianRegressionModels.jl
+++ src/BayesianRegressionModels.jl
@@ -1,3 +1,5 @@
+module BayesianRegressionModels
+
 ## For the regression distribution ##
 using Distributions
 using LinearAlgebra
@@ -5,12 +7,6 @@ using Random
 using PDMats #This gives efficient computation for positive definite matrices, used for the LKJCholesky stuff
 using DimensionalData #This allows for named dimensions in arrays
 using DimensionalData: @dim
-
-## For the Turing model ##
-using Turing
-using FlexiChains
-
-
 
 include("1_RegressionSpecifications.jl")
 
@@ -22,8 +18,6 @@ include("4_RegressionPredictors.jl")
 
 include("5_linear_combination.jl")
 
-include("6_TuringExt.jl")
-
 include("7_basis_expansions.jl")
 
 include("8_interaction_operators.jl")
@@ -31,3 +25,5 @@ include("8_interaction_operators.jl")
 include("9_regression_submodels.jl")
 
 include("10_formula.jl")
+
+end
