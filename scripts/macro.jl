6baf94f58950c9380de51ae88ba64bfc2f32ba95
Nikolas Siccha <nikolassiccha@gmail.com>
2026-03-04 13:12:51 +0100

    first parsing prototype

--- scripts/macro.jl
+++ scripts/macro.jl
@@ -23,7 +23,7 @@ end
 
 # With a single dataframe, the model definition could look as follows (which would lower to the "obvious" syntax):
 model = @brm begin 
-    BMI ~ Normal(BMI_measured, 1)
+    # BMI ~ Normal(BMI_measured, 1)
     Age_first, Age_second = ploynomial_expand(Age; order=2)
     performance_mean ~ 1 + Age_first * Treatment + Age_second + (1 + Treatment | Subject) + (1 + Age_first | Experimenter)
     log(performance_sd) ~ 1 + AGE * BMI + max(Age, BMI) + (1 + Age * BMI | Subject)
@@ -34,4 +34,132 @@ model = @brm begin
             Block2=>[Age, BMI]
         )
     end
-end 
+end
+
+
+macro brm(x)
+    esc(_brm(x))
+end
+begin
+function ensurecols end
+function maybedists end
+# isxcall(x) = Meta.isexpr(x, :call)
+isxcall(x, f) = Meta.isexpr(x, :call) && x.args[1] == f
+fixcall(x) = x
+fixcall(x::Expr) = if Meta.isexpr(x, :call)
+    f = x.args[1]
+    pargs = []
+    args = []
+    for arg in fixcall.(x.args[2:end])
+        if Meta.isexpr(arg, :parameters)
+            append!(pargs, arg.args)
+        else
+            push!(args, arg)
+        end
+    end
+    if length(pargs) > 0
+        Expr(x.head, f, Expr(:parameters, pargs...), args...)
+    else
+        Expr(x.head, f, args...)
+    end
+else
+    Expr(x.head, fixcall.(x.args)...)
+end
+xensurecols(x::Expr) = if x.head == :call
+    Expr(:call, ensurecols, x.args...) |> fixcall
+else 
+    dump(x)
+    error("Don't know how to handle xensurecols($x)!")
+end
+    parse!(x::LineNumberNode; info) = x
+parse!(x::Expr; info) = if x.head == :block
+    Expr(:block, parse!.(x.args; info)...)
+elseif x.head == :(=)
+    lhs, rhs = x.args
+    lhs = parse_assignment_lhs!(lhs; info)
+    rhs = parse_assignment_rhs!(rhs; info)
+    Expr(:(=), lhs, xensurecols(rhs))
+elseif isxcall(x, :~)
+    _, lhs, rhs = x.args
+    lhs = parse_sampling_lhs!(lhs; info)
+    rhs = parse_sampling_rhs!(rhs; info)
+    if isxcall(rhs, ensurecols)
+        :($lhs = $maybedists(;force=isdata($lhs))($(rhs.args[2:end]...)))
+    else
+        :($lhs = $maybedists(;force=isdata($lhs))($(rhs)))
+    end
+else
+    dump(x)
+    error("Don't know how to handle parse!($x)!")
+end
+parse_assignment_lhs!(x::Symbol; info) = (get!(info.alllocals, x, :local); x)
+parse_assignment_lhs!(x::Expr; info) = begin 
+    @assert Meta.isexpr(x, (:tuple, :vect))
+    args = parse_assignment_lhs!.(x.args; info)
+    Expr(x.head, args...)
+end
+parse_assignment_rhs!(x::Symbol; info) = (get!(info.alllocals, x, :nonlocal); x)
+parse_assignment_rhs!(x::Expr; info) = if x.head == :call
+    Expr(:call, x.args[1], parse_assignment_rhs!.(x.args[2:end]; info)...)
+elseif Meta.isexpr(x, :parameters)
+    x
+else
+    dump(x)
+    error("Don't know how to handle parse_assignment_rhs!($x)!")
+end
+parse_sampling_lhs!(x::Symbol; info) = (get!(info.alllocals, x, :maybelocal); x)
+parse_sampling_lhs!(x::Expr; info) = if x.head == :call
+    Expr(:call, x.args[1], parse_sampling_lhs!.(x.args[2:end]; info)...)
+else 
+    @assert Meta.isexpr(x, (:tuple, :vect))
+    args = parse_sampling_lhs!.(x.args; info)
+    Expr(x.head, args...)
+end
+parse_sampling_rhs!(x::Number; info) = x
+parse_sampling_rhs!(x::Symbol; info) = (get!(info.alllocals, x, :nonlocal); x)
+parse_sampling_rhs!(x::Expr; info) = if x.head == :call
+    if x.args[1] in (:+, :*, :|)
+        Expr(:call, x.args[1], parse_sampling_rhs!.(x.args[2:end]; info)...)
+    else
+        xensurecols(parse_assignment_rhs!(x; info))
+    end
+elseif Meta.isexpr(x, :parameters)
+    x
+else
+    dump(x)
+    error("Don't know how to handle parse_sampling_rhs!($x)!")
+end
+using OrderedCollections
+_brm(x::Expr) = begin 
+    @assert x.head == :block
+    alllocals = OrderedDict{Symbol,Symbol}()
+    info = (;alllocals)
+    x = parse!(x; info)
+    nonlocals = [key for (key, value) in pairs(alllocals) if value == :nonlocal]
+    maybelocals = [key for (key, value) in pairs(alllocals) if value == :maybelocal]
+    locals = [key for (key, value) in pairs(alllocals) if value == :local]
+    init = quote
+        (;$(nonlocals...)) = data(__df__)
+        (;$(maybelocals...)) = maybedata(__df__)
+    end
+    finalize = :(BRM(;$(keys(alllocals)...)))
+    Expr(:(=), :(model(__df__)), Expr(:block, init, x.args..., finalize))
+end
+end
+@macroexpand @brm begin 
+    Age_first, Age_second = ploynomial_expand(Age; order=2)
+    performance_mean ~ 1 + Age_first * Treatment + Age_second + (1 + Treatment | Subject) + (1 + Age_first | Experimenter)
+    log(performance_sd) ~ 1 + Age * BMI + max(Age, BMI) + (1 + Age * BMI | Subject)
+    Performance ~ Normal(performance_mean, performance_sd)
+end
+
+
+# model(__df__) = begin
+#     (; Age, Treatment, Subject, Experimenter, BMI) = data(__df__)
+#     (; performance_mean, performance_sd, Performance) = maybedata(__df__)
+#     (Age_first, Age_second) = (ensurecols)(ploynomial_expand, Age; order=2)
+#     performance_mean = ((maybedists)(; force=isdata(performance_mean)))(1 + Age_first * Treatment + Age_second + ((1 + Treatment) | Subject) + ((1 + Age_first) | Experimenter))
+#     log(performance_sd) = ((maybedists)(; force=isdata(log(performance_sd))))(1 + Age * BMI + (ensurecols)(max, Age, BMI) + ((1 + Age * BMI) | Subject))
+#     Performance = ((maybedists)(; force=isdata(Performance)))(Normal, performance_mean, performance_sd)
+#     BRM(; Age_first, Age_second, Age, performance_mean, Treatment, Subject, Experimenter, performance_sd, BMI, Performance)
+# end
